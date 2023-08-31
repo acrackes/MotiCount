df5b0657f7f73f41977eed399649543681c7ae95
AcrackSoft <games@acracksoft.es>
2023-08-31 21:50:33 +0200

    Actualizada la interfaz, creada la función de calculo de la diferencia entre la fecha actual y la que introducimos.

--- app/src/main/java/es/acracksoft/moticount/MainActivity.kt
+++ app/src/main/java/es/acracksoft/moticount/MainActivity.kt
@@ -18,23 +18,49 @@
 package es.acracksoft.moticount
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import es.acracksoft.moticount.ui.theme.MotiCountTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +69,8 @@ class MainActivity : ComponentActivity() {
             MotiCountTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Content()
                 }
@@ -53,34 +80,233 @@ class MainActivity : ComponentActivity() {
 }
 
 //Contenedor principal de la aplicación
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content() {
+    //Variables
+    var dias by remember { mutableStateOf(0) }
+    var horas by remember { mutableStateOf(0) }
+    var minutos by remember { mutableStateOf(0) }
+    var segundos by remember { mutableStateOf(0) }
+    var frase by remember { mutableStateOf("") }
+
+    //Calendario
+    val calendar = java.util.Calendar.getInstance()
+    // set the initial date
+    val datePickerState: DatePickerState  = rememberDatePickerState(initialSelectedDateMillis = calendar.timeInMillis)
+    var showDatePicker: Boolean by remember { mutableStateOf(false) }
+    var selectedDate: Long by remember { mutableLongStateOf(calendar.timeInMillis) }
+    var actualDate: Long by remember { mutableLongStateOf(calendar.timeInMillis) }
+    var finishDate: Long by remember { mutableLongStateOf(calendar.timeInMillis) }
+
+    frase = "Esto es una frase muy larga para que veamos que tal se " +
+            "puede mostrar la frase en todo su explendor" +
+            " y que se vea bien"
+
     //Contenido de la aplicación
-    Scaffold() { padding ->
+    Scaffold(modifier = Modifier.padding(16.dp)) { padding ->
         Column(modifier = Modifier.padding(padding)) {
-            StyleCard(1, 200, 200)
+            //Título
+            StyteText(
+                "TE QUEDAN", 50,
+                Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+            //Dias
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                StyteText("Días:", 60, Modifier.align(Alignment.CenterVertically))
+                StyleCard(dias, 150, 100, 70)
+            }
+            //Horas
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                StyteText("Horas:", 50, Modifier.align(Alignment.CenterVertically))
+                StyleCard(horas, 150, 100, 70)
+            }
+            //Minutos
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                StyteText("Minutos:", 40, Modifier.align(Alignment.CenterVertically))
+                StyleCard(minutos, 150, 100, 70)
+            }
+            //Segundos
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                StyteText("Segundos:", 30, Modifier.align(Alignment.CenterVertically))
+                StyleCard(segundos, 150, 100, 70)
+            }
+            //Espacio
+            Spacer(modifier = Modifier.height(5.dp))
+            //genera una linea sepadora
+
+            //Frases
+            StyteText(
+                frase, 20,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .align(Alignment.Start)
+            )
+
+            //Botón para seleccionar fecha
+            MyDatePicker(
+                datePickerState,
+                showDatePicker,
+                selectedDate,
+                onShowDatePickerChange = { showDatePicker = it },
+                onSelectedDateChange = { selectedDate = it }
+            )
+            finishDate = selectedDate
+
+            Log.d("TAG", "Selected date: $selectedDate")
+            Log.d("TAG", "Actual date: $actualDate")
+            Log.d("TAG", "Finish date: $finishDate")
+
+            var timeDifference = calculateTimeDifference(initDate = actualDate, endDate = finishDate)
+            Log.d("TAG", "Time difference: $timeDifference")
+
+            //MyDatePicker()
+            /*ExtendedFloatingActionButton(
+                modifier = Modifier.align(Alignment.End),
+                onClick = { MyDatePicker() },
+                icon = { Icon(Icons.Filled.Add, "Seleccione fecha") },
+                text = { Text(text = "Seleccione fecha") },
+            )*/
+
         }
 
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun StyleCard(dato: Int, ancho: Int, alto: Int) {
-    Card(
-        modifier = Modifier.size(width = ancho.dp, height = alto.dp)
+fun MyDatePicker(
+    datePickerState: DatePickerState,
+    showDatePicker: Boolean,
+    selectedDate: Long,
+    onShowDatePickerChange: (Boolean) -> Unit,
+    onSelectedDateChange: (Long) -> Unit
+) {
+    if (showDatePicker) {
+        DatePickerDialog(
+            onDismissRequest = {
+                false.also { onShowDatePickerChange(it) }
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    false.also { onShowDatePickerChange(it) }
+                    datePickerState.selectedDateMillis!!.also { onSelectedDateChange(it) }
+                }) {
+                    Text(text = "Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    false.also { onShowDatePickerChange(it) }
+                }) {
+                    Text(text = "Cancel")
+                }
+            }
+        ) {
+            DatePicker(
+                state = datePickerState
+            )
+        }
+    }
+
+    Button(
+        onClick = {
+            true.also { onShowDatePickerChange(it) }
+        }
     ) {
-        Column(Modifier.fillMaxSize()) {
+        Text(text = "Selecciona fecha")
+    }
+
+    val formatter = SimpleDateFormat("dd MMMM yyyy", Locale.ROOT)
+    Text(
+        text = "Selected date: ${formatter.format(Date(selectedDate))}"
+    )
+    Log.d("TAG", "Selected date: $selectedDate")
+
+}
+
+@Composable
+fun StyteText(dato: String, sizefont: Int, modifier: Modifier) {
+    Text(
+        text = dato.toString(),
+        modifier = modifier.padding(16.dp),
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = sizefont.sp,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun StyleCard(dato: Int, ancho: Int, alto: Int, sizefont: Int) {
+    Card(
+        modifier = Modifier.size(width = ancho.dp, height = alto.dp),
+        shape = MaterialTheme.shapes.small,
+        elevation = CardDefaults.cardElevation(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = dato.toString(),
                 modifier = Modifier.align(Alignment.CenterHorizontally),
-                fontSize = 40.dp,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                fontSize = sizefont.sp,
+                textAlign = TextAlign.Center,
             )
         }
 
     }
 
 }
+
+//Función que calcula el tiempo transcurrido entre dos fechas
+fun calculateTimeDifference(initDate: Long, endDate: Long): String {
+    val timeDifference = endDate - initDate
+
+    val days = TimeUnit.MILLISECONDS.toDays(timeDifference)
+    val hours = TimeUnit.MILLISECONDS.toHours(timeDifference) % 24
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(timeDifference) % 60
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(timeDifference) % 60
+
+    //Actualizamos las variables de tiempo
+    dias = days
+    horas = hours
+    minutos = minutes
+    segundos = seconds
+
+    return "Days: $days, Hours: $hours, Minutes: $minutes, Seconds: $seconds"
+}
+
+
 
 @Preview(showSystemUi = true)
 @Composable
---
+++ app/src/main/java/es/acracksoft/moticount/datos/datos.kt
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2023.
+ * Create by: Acrack
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package es.acracksoft.moticount.datos
+
+data class FraseMotivadora(val frases: String)
+
+val frasesMotivadoras = listOf(
+    FraseMotivadora("El éxito es la suma de pequeños esfuerzos repetidos día tras día."),
+    FraseMotivadora("Cada paso te acerca más a tu meta, ¡sigue adelante!"),
+    FraseMotivadora("La única forma de hacer un gran trabajo es amar lo que haces."),
+    FraseMotivadora("Tú eres la fuerza que necesitas para alcanzar tus sueños."),
+    FraseMotivadora("Las oportunidades no ocurren, las creas."),
+    FraseMotivadora("No importa cuánto avances, siempre estás más cerca de tu destino."),
+    FraseMotivadora("Las dificultades preparan a personas comunes para destinos extraordinarios."),
+    FraseMotivadora("El camino puede ser difícil, pero cada paso cuenta."),
+    FraseMotivadora("Tu determinación superará cualquier obstáculo en tu camino."),
+    FraseMotivadora("Cree en ti mismo y estarás a mitad de camino."),
+    FraseMotivadora("El éxito es la suma de tus decisiones correctas."),
+    FraseMotivadora("La perseverancia es la clave que desbloquea tus logros."),
+    FraseMotivadora("Cada día es una nueva oportunidad para avanzar."),
+    FraseMotivadora("Las excusas no te acercan a tus metas, los esfuerzos sí."),
+    FraseMotivadora("Los desafíos son solo oportunidades enmascaradas."),
+    FraseMotivadora("Con cada hora, estás un paso más cerca de la victoria."),
+    FraseMotivadora("Los sueños se convierten en realidad cuando la pasión los impulsa."),
+    FraseMotivadora("La paciencia es la llave que abre las puertas del éxito."),
+    FraseMotivadora("Nunca subestimes el poder de tu propia determinación."),
+    FraseMotivadora("Enfócate en el progreso, no en la perfección."),
+    FraseMotivadora("Tus metas te esperan al final de tu zona de confort."),
+    FraseMotivadora("El esfuerzo que pones hoy cosechará recompensas mañana."),
+    FraseMotivadora("Tú eres el autor de tu propio éxito, escribe una gran historia."),
+    FraseMotivadora("El camino puede ser empinado, pero la cima vale la pena."),
+    FraseMotivadora("Cada día es una página en blanco, ¡llénala con tus logros!"),
+    FraseMotivadora("La autodisciplina es el puente entre tus metas y logros."),
+    FraseMotivadora("Mantén tus ojos en la meta, no en los obstáculos."),
+    FraseMotivadora("El único fracaso real es no intentarlo en absoluto."),
+    FraseMotivadora("Las estrellas brillan más en la oscuridad, al igual que tus logros."),
+    FraseMotivadora("No te detengas hasta sentirte orgulloso."),
+    FraseMotivadora("Los pequeños pasos te llevan lejos si los das con determinación."),
+    FraseMotivadora("El optimismo es la fe que conduce al éxito."),
+    FraseMotivadora("El camino puede ser largo, pero cada paso te acerca más."),
+    FraseMotivadora("Cada día es una oportunidad para ser un paso más cercano a tu objetivo."),
+    FraseMotivadora("La motivación te pone en marcha, el hábito te mantiene en movimiento."),
+    FraseMotivadora("Tus esfuerzos de hoy son los frutos de mañana."),
+    FraseMotivadora("Cada día es una nueva oportunidad para acercarte a tus sueños."),
+    FraseMotivadora("El éxito es la suma de pequeños esfuerzos diarios."),
+    FraseMotivadora("Cada hora es un recordatorio de que estás un paso más cerca."),
+    FraseMotivadora("El progreso puede ser lento, pero siempre es significativo."),
+    FraseMotivadora("La disciplina es el puente entre tus metas y tus logros."),
+    FraseMotivadora("No mires atrás, estás más cerca de tu objetivo de lo que piensas."),
+    FraseMotivadora("Los sueños se logran cuando te atreves a actuar."),
+    FraseMotivadora("Cada día es una oportunidad para acercarte un poco más."),
+    FraseMotivadora("Cada paso te acerca a donde quieres estar."),
+    FraseMotivadora("El esfuerzo constante te llevará a resultados sorprendentes."),
+    FraseMotivadora("La perseverancia es la clave del éxito en cualquier camino."),
+    FraseMotivadora("El camino puede ser difícil, pero cada paso cuenta."),
+    FraseMotivadora("No te rindas, cada paso te acerca a la cima."),
+    FraseMotivadora("Tus esfuerzos diarios construyen tus éxitos futuros."),
+    FraseMotivadora("El éxito se logra un paso a la vez, ¡sigue adelante!"),
+    FraseMotivadora("La consistencia es la clave para transformar sueños en realidad."),
+    FraseMotivadora("La voluntad de ganar te lleva a la victoria."),
+    FraseMotivadora("Cada hora es un nuevo comienzo en tu viaje hacia el éxito."),
+    FraseMotivadora("No cuentes los días, haz que los días cuenten."),
+    FraseMotivadora("Tú eres la clave de tu propio éxito."),
+    FraseMotivadora("El éxito es la suma de pequeños esfuerzos continuos."),
+    FraseMotivadora("Cada paso te acerca a donde quieres estar."),
+    FraseMotivadora("Los desafíos te hacen más fuerte y te acercan a tus objetivos."),
+    FraseMotivadora("El éxito está en la perseverancia y la determinación."),
+    FraseMotivadora("No importa cuánto tardes, siempre estarás más cerca."),
+    FraseMotivadora("Cada hora cuenta en tu viaje hacia el éxito."),
+    FraseMotivadora("La pasión y el esfuerzo son la clave para alcanzar tus sueños."),
+    FraseMotivadora("No retrocedas, estás más cerca de lo que piensas."),
+    FraseMotivadora("Tus sueños merecen cada esfuerzo que pongas."),
+    FraseMotivadora("Cada paso te lleva más cerca de tus metas."),
+    FraseMotivadora("El viaje puede ser difícil, pero vale la pena."),
+    FraseMotivadora("La dedicación es la clave para alcanzar tus sueños."),
+    FraseMotivadora("Cada hora te acerca un poco más a tu objetivo."),
+    FraseMotivadora("La determinación te lleva de donde estás a donde quieres estar."),
+    FraseMotivadora("Cada día es una oportunidad para avanzar hacia tus metas."),
+    FraseMotivadora("El éxito es el resultado de pequeñas acciones realizadas todos los días."),
+    FraseMotivadora("Tú eres la fuerza que te lleva a la cima."),
+    FraseMotivadora("Cada hora es un paso adelante hacia el éxito."),
+    FraseMotivadora("Los obstáculos son solo escalones hacia tus logros."),
+    FraseMotivadora("La perseverancia es la clave para superar cualquier desafío."),
+    FraseMotivadora("Cada paso es un avance hacia tu objetivo."),
+    FraseMotivadora("No te rindas, estás más cerca de lo que crees."),
+    FraseMotivadora("El esfuerzo constante te acerca cada vez más a tus metas."),
+    FraseMotivadora("Cada hora es un recordatorio de que estás avanzando."),
+    FraseMotivadora("La pasión te impulsa, la perseverancia te mantiene."),
+    FraseMotivadora("Tus sueños valen la pena cada sacrificio."),
+    FraseMotivadora("El éxito se construye paso a paso."),
+    FraseMotivadora("Cada día te brinda una oportunidad para acercarte más."),
+    FraseMotivadora("El progreso puede ser lento, pero es valioso."),
+    FraseMotivadora("No mires atrás, estás más cerca de lo que deseas."),
+    FraseMotivadora("Cada paso te acerca a tus metas y sueños."),
+    FraseMotivadora("El éxito es el resultado de tus esfuerzos diarios."),
+    FraseMotivadora("Cada hora es una oportunidad para avanzar."),
+    FraseMotivadora("La perseverancia te llevará a la cima."),
+    FraseMotivadora("El éxito se trata de mantenerse enfocado en tus objetivos."),
+    FraseMotivadora("Cada día es una nueva oportunidad para avanzar."),
+    FraseMotivadora("Tú eres el conductor de tu propio éxito."),
+    FraseMotivadora("El camino puede ser desafiante, pero cada paso es valioso."),
+    FraseMotivadora("Cada hora te acerca un poco más a tu destino."),
+    FraseMotivadora("La pasión te da el impulso, la determinación te lleva a la cima."),
+    FraseMotivadora("Cada día es un nuevo paso hacia tus logros."),
+    FraseMotivadora("La disciplina es el puente entre metas y éxitos."),
+    FraseMotivadora("Cada hora cuenta en el camino hacia el éxito."),
+    FraseMotivadora("El progreso constante te lleva a resultados asombrosos.")
+)
+
+
+/*
+¡Claro! Aquí tienes una descripción para la app "MotiCount" que podrías usar en Google Play:
+
+"MotiCount: Encuentra inspiración en cada momento. ¿Tienes una fecha importante en el horizonte?
+Ya sea un evento especial, un plazo crucial o un objetivo personal, MotiCount te acompaña en cada
+segundo del camino. Configura la fecha y observa cómo el contador regresivo te acerca a tu meta.
+Pero eso no es todo: ¡cada hora, MotiCount te brinda un impulso de ánimo con mensajes motivacionales
+únicos! Mantén tu espíritu en alto a medida que avanzas hacia tus objetivos. Descarga MotiCount y
+permite que cada momento cuente en tu búsqueda de logros."
+ */
